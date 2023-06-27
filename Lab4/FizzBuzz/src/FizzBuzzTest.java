3be46caba229c00db4cac80e59542d00e47c8b74
kaiser-brice <bricengnawo@gmail.com>
2023-06-27 17:29:25 -0400

    Divisibility by 3 Test Failed

--- Lab4/FizzBuzz/src/FizzBuzzTest.java
+++ Lab4/FizzBuzz/src/FizzBuzzTest.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
     @Test
-    void process() {
+    public void checkDivisibilityBy3() {
+        Assertions.assertEquals("Fizz", fizzBuzz.process(9));
+        Assertions.assertEquals("Fizz", fizzBuzz.process(12));
     }
+
+
 }
