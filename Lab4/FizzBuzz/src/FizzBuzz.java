56bbf3de3d95c60c84b651ad4169287f2f104acd
kaiser-brice <bricengnawo@gmail.com>
2023-06-27 17:17:16 -0400

    Creation of TDD for FizzBuzz

---
+++ Lab4/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,16 @@
+/**
+ * @Author: Brice Joan Ngnawo SOh - nso087@uottawa.ca
+ * ---------------------------------------------------
+ * FizzBuzz is a program which takes an integer, n:
+ * returns Fizz if n is divisble by 3
+ * returns Buzz if n is divisible by 5
+ * returns FIzzBuzz if n is divisible by 3 and 5
+ * returns n is n is not divisible neither by 3 and 5
+ */
+public class FizzBuzz {
+    public FizzBuzz() {
+    }
+    public String process(int n){
+        return null;
+    }
+}
