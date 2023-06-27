dc7236966b5502be3988006ec90cd2423fcc8f11
kaiser-brice <bricengnawo@gmail.com>
2023-06-27 17:41:00 -0400

    Divisibility by 3 and 5 Test Passed

--- Lab4/FizzBuzz/src/FizzBuzz.java
+++ Lab4/FizzBuzz/src/FizzBuzz.java
@@ -11,12 +11,16 @@ public class FizzBuzz {
     public FizzBuzz() {
     }
     public String process(int n){
-        if(n % 3 == 0){
+        if(n % 3 == 0 && n % 5 != 0){
             return "Fizz";
         }
 
-        if(n % 5 == 0){
+        if(n % 5 == 0 && n % 3 != 0){
             return "Buzz";
+        }
+
+        if(n % 15 == 0){
+            return "FizzBuzz";
         }
         return null;
     }
